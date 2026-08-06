063dde436e64721e35bb92ab1b5bf514c8d74b3f
Ahmed Ghalib <116951136+a-ghalib@users.noreply.github.com>
2026-08-06 23:10:35 +0300

    Remove commented code and clean up menu functions

--- Js/min.js
+++ Js/min.js
@@ -7,17 +7,3 @@ function menuBtn() {
   });
 }
 menuBtn();
-
-// function closeX() {
-//   document.body.addEventListener("click", () => {
-//     console.log("CloseX")
-//   });
-// }
-
-// let newOne = ["1", " 2", "3 ", "4", "5", "6 ", "7", "8", "9 ", "10"];
-// let newTwo = ["Ahmed", "Rahma", "Mohamed"];
-// let newThree = [];
-// console.log(newThree.concat(newTwo.join(" ").substring(0, 11).split(" ")));
-
-// let newFour = [1, 2, 4, 68, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-// console.log(newFour.map(Number).toFixed(2));
